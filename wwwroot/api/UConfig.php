25b357d1de65ae9a7786494ae949a3d022f72206
1415699306 <1415699306@qq.com>
2014-11-13 15:23:31 +0800

    no message

---
+++ wwwroot/api/UConfig.php
@@ -0,0 +1,17 @@
+<?php
+define('UC_CONNECT', 'mysql');
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'lehuidb');
+define('UC_DBPW', '@lehuidb_123546');
+define('UC_DBNAME', 'lehuidb');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`lehuidb`.pre_ucenter_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', '4c9dTPefDC3J0AkZe4TVz0NCdATxURoqI7BA9+o');
+define('UC_API', 'http://quanzi.lfeel.com/uc_server');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '');
+define('UC_APPID', '2');
+define('UC_PPP', '20');
+
+
